025dcc18b56b8358d11223babc51aeaeb1a0e333
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-01-06 17:47:31 +0900

    2.4 (done)

--- Programming with Python/Building Python Web Apps with Flask/2.4/app.py
+++ Programming with Python/Building Python Web Apps with Flask/2.4/app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
 
+import json
+
 app     = Flask(__name__)
 
 @app.route('/')
 def root():
-    return "test"
+    return "/"
+
+@app.route("/character")
+def character():
+    characters = dict()
+    with open("data/characters.json") as file:
+        characters  = json.load(file)
+
+    return render_template("character.html", characters=characters)
+
 
 if __name__ == "__main__":
     app.run()
